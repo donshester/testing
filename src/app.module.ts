6351206e1398672739c2459cf74af0926fae98ed
Vladislav Stepanov <vlad.stepanov.2003@bk.ru>
2023-05-06 22:53:34 +0300

    ADD: typeorm db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeormModule } from './typeorm/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TypeormModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeorm/typeorm.config.ts
@@ -0,0 +1,23 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import * as process from 'process';
+import { join } from 'path';
+
+config({ path: join(process.cwd(), '.env') });
+const configService = new ConfigService();
+
+const options = (): DataSourceOptions => {
+  return {
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(<string>process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    type: 'postgres',
+    logging: configService.get('IS_PROD') === 'false',
+    entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
+    migrations: ['dist/migrations/*.{ts,js}'],
+  };
+};
+export const appDataSource = new DataSource(options());
