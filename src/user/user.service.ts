42cd92b3e02c5515fa38e6734ed852094e6ca670
Vladislav Stepanov <vlad.stepanov.2003@bk.ru>
2023-05-07 18:15:15 +0300

    ADD CRUD methods for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { UserEntity } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-  async createUser(userDto: CreateUserDto): Promise<UserEntity> {
-    return;
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = await this.userRepository.create({ ...createUserDto });
+    return await this.userRepository.save(newUser);
   }
 
   async getUserByEmail(email: string): Promise<UserEntity> {
-    return;
+    const user = await this.userRepository.findOneBy({ email: email });
+    if (!user) {
+      throw new NotFoundException(`User with email ${user.email} not found.`);
+    }
+
+    return user;
+  }
+  async updateUserByEmail(
+    email: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserEntity> {
+    const user = await this.userRepository.findOneBy({ email: email });
+
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+    return await this.userRepository.save({ ...user, ...updateUserDto });
   }
 
-  async updateUserByEmail(email: string, updateUserDto: UpdateUserDto) {}
-  // ): Promise<UserEntity> {}
+  async deleteUserByEmail(email: string): Promise<void> {
+    const result = await this.userRepository.delete({ email });
 
-  async deleteUserByEmail(email: string): Promise<void> {}
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+  }
 }
